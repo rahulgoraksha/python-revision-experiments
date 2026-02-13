700322b0bbf5de5bf37e892f9e0e299bdf2fa5f4
rahulgoraksha <rahulgoraksha@gmail.com>
2026-02-13 22:37:09 +0530

    stddeviation commit

---
+++ machine_learning/2_MeanMedianMode.py
@@ -0,0 +1,13 @@
+# data set: [99,96,85,35,36]
+
+# In ML and in math there are three value basically that are actually interested:
+
+# Mean : Average Value
+# Median : Midpoint Value
+# Mode : Most common value
+
+# in these example we have speed of 13 cards
+speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+# Mean: to calculate the mean- sum of all number / divide by its total count
+
