0d48ef17a55792b1000ae04b7910d8625bda423a
rahulgoraksha <rahulgoraksha@gmail.com>
2026-02-14 15:22:17 +0530

    supervised commit

---
+++ 3.machine_learning/1_introML.py
@@ -0,0 +1,75 @@
+# Machine Learning is making the computer to learn from the studying the data & statistic. This is a program that analysis data and learns to predict the outcome.
+
+# Data type: 
+# A. Numerical - 
+# 1. Discrete Data(number limited to integer: car passing through toll) 
+# 2. Continous Data (prize of item)
+
+# B. Categorical - the values that can't be count or measure like colour
+
+# C. Ordinal: similiar to categorical data but can be measured or count
+
+#---------------------------------------------------------------------------------------------
+
+# data set: [99,96,85,35,36]
+
+# In ML and in math there are three value basically that are actually interested:
+
+# Mean : Average Value
+# Median : Midpoint Value
+# Mode : Most common value
+
+# in these example we have speed of 13 cards
+speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+# Mean: to calculate the mean- sum of all number / divide by its total count
+
+
+# mean# the averarhe of the datatset
+
+# median# the midpoint of the dataset of even series, for odd deries the two valuse avg and that is waht for median
+
+# mode# the consistent number of dataset beign repeated again an again
+
+#---------------------------------------------------------------------------------------------
+
+# std devaition# types of deviation lowerr SD and Higher SD
+# Example:  Lower SDimport numpy as npspeed = [86,88,82,86,87,85,86]x = np.std(speed)print(x)
+
+# Example: Higher SDimport numpy as npspeed = [32,111,138,28,59,7]x = np.std(speed)print(x)
+
+# Variance : how the value are spread# if you take the square root of the Variance you will get the SD
+
+# standard Deviation : sigma
+
+# variance : sigma^2
+
+#---------------------------------------------------------------------------------------------
+
+# Big data distribution using matplotlib to plot the hist
+
+#---------------------------------------------------------------------------------------------
+
+# Normal data distribution: In numpy probablity theory this kind of data dist is known as normal data dist or Gaussian Data Dist
+
+# Normal data dist is also known as bell curve because of its bell shape
+
+#---------------------------------------------------------------------------------------------
+
+# scatter plot: diagram where each value od data are been plotted by means of dots
+
+#---------------------------------------------------------------------------------------------
+# ML for Linear Regression
+
+# Regression: use to find the relationship between the variable. In Machin learning & Statistcal modelling to predict the outcome of the future event
+
+# for example :
+# a = [ A,B,C,D,E]
+# b = [a,b,c,d,e] ..both are list , having alphabet in list is what the relation forming
+
+# Types of Regression:
+
+# Linear Regression: it basically use the relationship between the data to point to draw a straight line through all of them, this line can be use to predict the future
+
+
+# { scipy ; scitific python and draw line of linear regression }
